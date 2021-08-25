7ebdd5a6bccee78e2a116bdf15477506ea0258c8
jinhyeok <ekdrn3330@naver.com>
2021-08-25 18:22:15 +0900

    210825리액트몰연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Navbar,Container,Nav,NavDropdown,Button } from 'react-bootstrap';
 import './App.css';
+import data from './data.js';
 
 function App() {
+
+  let [shoes, shoes변경] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="background">
+        <h1>20% Season Off</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          dasdsadsadasdaskjdhaskjdhkjashdkjsa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <Button variant="primary">Primary</Button>
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 &amp; 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 &amp; 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 &amp; 가격</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
